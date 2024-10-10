de2559357779a777484c850cd02565cad0779d27
IgorSilvares <104534919+IgorSilvares@users.noreply.github.com>
2024-10-10 16:42:51 +0200

    data structure created

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def main():
     """
     The main entry point of the program. Initializes the MovieApp class with a StorageJson object and calls its run method.
     """
-    storage = StorageJson("igor_movies.json")
+    storage = StorageJson("data/igor_movies.json")
     movie_app = MovieApp(storage)
     movie_app.run()
 
--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 from movie_app import MovieApp
 from storage_csv import StorageCsv
 
-storage = StorageCsv('movies.csv')
+storage = StorageCsv('data/movies.csv')
 movie_app = MovieApp(storage)
 movie_app.run()
